774784013a1631672bdeb29c6edf47ba5cf7c8f9
adbenitez <asieldbenitez@gmail.com>
2023-07-22 13:10:42 +0200

    port to webxdc

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# JS13k 2018
+# Audio Dash
 
-Play the game: https://straker.github.io/js13k-2018/
+A WebXDC port of [Audio Dash](https://github.com/straker/js13k-2018)
+
+## Build the game
+
+```sh
+npm install
+npm run build
+```
+
+The game should then be in `AudioDash.xdc`.
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,46 @@
 const gulp = require('gulp');
 const size = require('gulp-size');
 const concat = require('gulp-concat');
-const rename = require('gulp-rename');
 const terser = require('gulp-terser');
 const plumber = require('gulp-plumber');
 const addsrc = require('gulp-add-src');
+const archiver = require('archiver')('zip');
+const fs = require('graceful-fs');
+const through = require('through2');
 
-gulp.task('build', function() {
+function build() {
   return gulp.src(['src/kontra.js', 'src/wavesurfer.js', 'src/globals.js', 'src/ui.js', 'src/*.js', '!src/main.js'])
     .pipe(addsrc.append('src/main.js'))
     .pipe(concat('index.js'))
-    .pipe(gulp.dest('.'))
-    .pipe(rename('dist.js'))
     .pipe(plumber())
     .pipe(terser())
     .pipe(plumber.stop())
+    .pipe(gulp.dest('.'));
+}
+
+function dist() {
+  const zip = fs.createWriteStream('AudioDash.xdc');
+  archiver.pipe(zip);
+  return gulp.src(['index.js', 'index.html', "manifest.toml", "icon.png"])
     .pipe(size({
       gzip: true
     }))
-    .pipe(gulp.dest('.'))
-});
+    .pipe(through.obj((file, encoding, cb) => {
+      archiver.append(file.contents, {
+        name: file.relative,
+        mode: file.stat
+      });
+      cb(null, file);
+    }, cb => {
+      zip.on('finish', cb);
+      archiver.finalize();
+    }));
+}
 
-gulp.task('dist', function() {
-  return gulp.src(['./dist.js', 'index.html'])
-    .pipe(size({
-      gzip: true
-    }))
-})
+function watch() {
+  gulp.watch('src/*.js', ['build']);
+}
 
-gulp.task('watch', function() {
-  gulp.watch('src/*.js', ['build', 'dist']);
-});
-
-gulp.task('default', ['build']);
+exports.build = build;
+exports.dist = dist;
+exports.default = gulp.series(build, dist);
